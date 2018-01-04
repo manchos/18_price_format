aaca51eb08b086d4245e322f06542f39fe585cc9
manchos <vladm.ru@gmail.com>
2018-01-04 21:33:24 +0300

    renaming functions and variables

--- format_price.py
+++ format_price.py
@@ -2,22 +2,22 @@
 def format_price(price):
     if isinstance(price, str):
         price = price.replace(',', '.')
-    if price_validate(price):
+    if is_validate_price(price):
         str_price = str(float(price))
         before_dot, after_dot = str_price.split('.')
         return '{}{}'.format(get_before_dot_str(before_dot), get_str_after_dot(after_dot))
 
 
-def get_before_dot_str(str_before_dot):
+def get_before_dot_str(str_before_dot, digits_amount_in_part=3):
     length = len(str_before_dot)
-    if length > 3:
-        parts = len(str_before_dot) // 3
-        parts_list = list(range(0, parts+1))
+    if length > digits_amount_in_part:
+        parts = len(str_before_dot) // digits_amount_in_part
+        parts_list = list(range(parts+1))
         for i in parts_list:
-            parts_list[parts] = str_before_dot[length-3:length]
+            parts_list[parts] = str_before_dot[length-digits_amount_in_part:length]
             parts = parts-1
             if parts != 0:
-                length = length-3
+                length = length-digits_amount_in_part
         parts_list[0] = str_before_dot[:length]
         return ' '.join(parts_list)
     else:
@@ -30,7 +30,7 @@ def get_str_after_dot(str_after_dot):
     return '.{}'.format(str_after_dot)
 
 
-def price_validate(price):
+def is_validate_price(price):
     try:
         float(price)
     except (ValueError, TypeError):
