9d50941586958c9bbc03bebff0bb564d8987e106
manchos <vladm.ru@gmail.com>
2018-01-04 22:14:40 +0300

    rename variable

--- format_price.py
+++ format_price.py
@@ -5,26 +5,26 @@ def format_price(price):
     if is_validate_price(price):
         str_price = str(float(price))
         before_dot, after_dot = str_price.split('.')
-        return '{}{}'.format(get_before_dot_str(before_dot), get_str_after_dot(after_dot))
+        return '{}{}'.format(get_format_before_dot_str(before_dot), get_after_dot_str(after_dot))
 
 
-def get_before_dot_str(str_before_dot, digits_amount_in_part=3):
-    length = len(str_before_dot)
+def get_format_before_dot_str(before_dot_str, digits_amount_in_part=3):
+    length = len(before_dot_str)
     if length > digits_amount_in_part:
-        parts = len(str_before_dot) // digits_amount_in_part
+        parts = len(before_dot_str) // digits_amount_in_part
         parts_list = list(range(parts+1))
-        for i in parts_list:
-            parts_list[parts] = str_before_dot[length-digits_amount_in_part:length]
+        for part in parts_list:
+            parts_list[parts] = before_dot_str[length - digits_amount_in_part:length]
             parts = parts-1
             if parts != 0:
                 length = length-digits_amount_in_part
-        parts_list[0] = str_before_dot[:length]
+        parts_list[0] = before_dot_str[:length]
         return ' '.join(parts_list)
     else:
-        return str_before_dot
+        return before_dot_str
 
 
-def get_str_after_dot(str_after_dot):
+def get_after_dot_str(str_after_dot):
     if int(str_after_dot) == 0:
         return ''
     return '.{}'.format(str_after_dot)
@@ -39,7 +39,7 @@ def is_validate_price(price):
 
 
 if __name__ == '__main__':
-    price = input('Enter the price to process (e.g. "3245.000000"): ')
+    price = input('Enter the price to process: ')
     try:
         formatted_price = format_price(price)
     except ValueError as error:
